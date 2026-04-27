6f948b8d4ecbcd397605c91a97e69165452fc1f0
Prince Diana <princeandrewdiana@gmail.com>
2026-04-28 00:04:54 +0800

    Cleanup controllers

--- src/main/java/com/princediana/airline_app/controller/AirportController.java
+++ src/main/java/com/princediana/airline_app/controller/AirportController.java
@@ -29,12 +29,10 @@ public class AirportController {
 	
 		List<Airport> airports;
 
-	    // 👉 INITIAL LOAD (NO FILTER)
+	    // INITIAL LOAD (NO FILTER)
 	    if ((country == null || country.isEmpty()) &&
 	        (city == null || city.isEmpty())) {
-
 	        airports = airLabsService.getAirports("", ""); // default fetch
-
 	    } else {
 	        // FILTERED
 	        airports = airLabsService.getAirports(country, city);
--- src/main/java/com/princediana/airline_app/controller/HomeController.java
+++ src/main/java/com/princediana/airline_app/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.princediana.airline_app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
